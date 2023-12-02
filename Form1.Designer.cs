efe1330e755ce37b1b976495e35fd5cd6adc5417
Welcolenus <w58.cemaly@gmail.com>
2023-12-02 23:07:05 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace Veritabanı
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.oGRENCIBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.database1DataSet = new Veritabanı.Database1DataSet();
+            this.oGRENCITableAdapter = new Veritabanı.Database1DataSetTableAdapters.OGRENCITableAdapter();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Ad = new System.Windows.Forms.TextBox();
+            this.Soyad = new System.Windows.Forms.TextBox();
+            this.Telefon = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.ID = new System.Windows.Forms.MaskedTextBox();
+            this.Ara = new System.Windows.Forms.Button();
+            this.ShowAll = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.oGRENCIBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(346, 22);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(442, 169);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.DoubleClick += new System.EventHandler(this.dataGridView1_DoubleClick);
+            // 
+            // oGRENCIBindingSource
+            // 
+            this.oGRENCIBindingSource.DataMember = "OGRENCI";
+            this.oGRENCIBindingSource.DataSource = this.database1DataSet;
+            // 
+            // database1DataSet
+            // 
+            this.database1DataSet.DataSetName = "Database1DataSet";
+            this.database1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // oGRENCITableAdapter
+            // 
+            this.oGRENCITableAdapter.ClearBeforeFill = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Id:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Adı:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Soyadı:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(39, 134);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Telefon:";
+            // 
+            // Ad
+            // 
+            this.Ad.Location = new System.Drawing.Point(91, 57);
+            this.Ad.Name = "Ad";
+            this.Ad.Size = new System.Drawing.Size(100, 20);
+            this.Ad.TabIndex = 6;
+            this.Ad.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Soyad
+            // 
+            this.Soyad.Location = new System.Drawing.Point(91, 93);
+            this.Soyad.Name = "Soyad";
+            this.Soyad.Size = new System.Drawing.Size(100, 20);
+            this.Soyad.TabIndex = 7;
+            // 
+            // Telefon
+            // 
+            this.Telefon.Location = new System.Drawing.Point(91, 131);
+            this.Telefon.Name = "Telefon";
+            this.Telefon.Size = new System.Drawing.Size(100, 20);
+            this.Telefon.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(244, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(244, 57);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Sil";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(244, 93);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Güncelle";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // ID
+            // 
+            this.ID.Location = new System.Drawing.Point(92, 22);
+            this.ID.Mask = "00000";
+            this.ID.Name = "ID";
+            this.ID.Size = new System.Drawing.Size(100, 20);
+            this.ID.TabIndex = 12;
+            this.ID.ValidatingType = typeof(int);
+            // 
+            // Ara
+            // 
+            this.Ara.Location = new System.Drawing.Point(244, 131);
+            this.Ara.Name = "Ara";
+            this.Ara.Size = new System.Drawing.Size(75, 23);
+            this.Ara.TabIndex = 13;
+            this.Ara.Text = "Ara";
+            this.Ara.UseVisualStyleBackColor = true;
+            this.Ara.Click += new System.EventHandler(this.Ara_Click);
+            // 
+            // ShowAll
+            // 
+            this.ShowAll.Location = new System.Drawing.Point(244, 161);
+            this.ShowAll.Name = "ShowAll";
+            this.ShowAll.Size = new System.Drawing.Size(75, 38);
+            this.ShowAll.TabIndex = 14;
+            this.ShowAll.Text = "Tümünü Göster";
+            this.ShowAll.UseVisualStyleBackColor = true;
+            this.ShowAll.Click += new System.EventHandler(this.ShowAll_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ShowAll);
+            this.Controls.Add(this.Ara);
+            this.Controls.Add(this.ID);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Telefon);
+            this.Controls.Add(this.Soyad);
+            this.Controls.Add(this.Ad);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.oGRENCIBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private Database1DataSet database1DataSet;
+        private System.Windows.Forms.BindingSource oGRENCIBindingSource;
+        private Database1DataSetTableAdapters.OGRENCITableAdapter oGRENCITableAdapter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox Ad;
+        private System.Windows.Forms.TextBox Soyad;
+        private System.Windows.Forms.TextBox Telefon;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.MaskedTextBox ID;
+        private System.Windows.Forms.Button Ara;
+        private System.Windows.Forms.Button ShowAll;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Veritabanı
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Database1.mdf;Integrated Security=True");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: Bu kod satırı 'database1DataSet.OGRENCI' tablosuna veri yükler. Bunu gerektiği şekilde taşıyabilir, veya kaldırabilirsiniz.
+            // this.oGRENCITableAdapter.Fill(this.database1DataSet.OGRENCI);
+
+            liste();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            // Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand komut = new SqlCommand("INSERT INTO OGRENCI(ADI,SOYADI,TELEFONU) VALUES(@AD,@SOYAD,@TELEFON)", conn);
+
+            komut.Parameters.AddWithValue("@AD", SqlDbType.NVarChar).Value = Ad.Text;
+            komut.Parameters.AddWithValue("@SOYAD", SqlDbType.NVarChar).Value = Soyad.Text;
+            komut.Parameters.AddWithValue("@TELEFON", SqlDbType.NVarChar).Value = Telefon.Text;
+            komut.ExecuteNonQuery();
+            conn.Close();
+            liste();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand komut = new SqlCommand("DELETE FROM OGRENCI WHERE ID=@ID", conn);
+
+            komut.Parameters.AddWithValue("@ID", SqlDbType.Int).Value = Convert.ToInt32(ID.Text);
+            komut.ExecuteNonQuery();
+            conn.Close();
+            liste();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            conn.Open();
+            SqlCommand komut = new SqlCommand("UPDATE OGRENCI SET ADI=@AD, SOYADI=@SOYAD, TELEFONU=@TELEFON WHERE ID=@ID", conn);
+
+            
+            komut.Parameters.AddWithValue("@AD", SqlDbType.NVarChar).Value = Ad.Text;
+            komut.Parameters.AddWithValue("@SOYAD", SqlDbType.NVarChar).Value = Soyad.Text;
+            komut.Parameters.AddWithValue("@TELEFON", SqlDbType.NVarChar).Value = Telefon.Text;
+            komut.Parameters.AddWithValue("@ID", SqlDbType.Int).Value = Convert.ToInt32(ID.Text);
+
+            komut.ExecuteNonQuery();
+            conn.Close();
+            liste();
+        }
+
+        private void Ara_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("SELECT * FROM OGRENCI WHERE ADI LIKE '%'+@AD+'%'", conn);
+            cmd.Parameters.AddWithValue("@AD", SqlDbType.NVarChar).Value = Ad.Text;
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(ds, "OGRENCI");
+            dataGridView1.DataSource = ds;
+            dataGridView1.DataMember = "OGRENCI";
+        }
+        public void liste()
+        {
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM OGRENCI", conn);
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(ds, "OGRENCI");
+            dataGridView1.DataSource = ds;
+            dataGridView1.DataMember = "OGRENCI";
+        }
+
+        private void dataGridView1_DoubleClick(object sender, EventArgs e)
+        {
+            ID.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            Ad.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            Soyad.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            Telefon.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+
+        }
+
+        private void ShowAll_Click(object sender, EventArgs e)
+        {
+            liste();
+        }
+    }
+}
